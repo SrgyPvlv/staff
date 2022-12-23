d2ea4d62170584427c66a2601c912ba739f09652
Sergey Pavlov <s.a.pavlov@mail.ru>
2022-12-24 00:02:10 +0300

    add @PathVariable

--- src/main/java/com/example/staff/controller/EmployeeEntityController.java
+++ src/main/java/com/example/staff/controller/EmployeeEntityController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class EmployeeEntityController {
 	private final EmployeeEntityService employeeEntityService;
 	
 	@GetMapping("/employee/{id}")
-	public ResponseEntity<EmployeeEntity> getEmployeeEntityById(Long id){
+	public ResponseEntity<EmployeeEntity> getEmployeeEntityById(@PathVariable Long id){
 		
 			return new ResponseEntity<>(employeeEntityService.getEmployeeEntityById(id),HttpStatus.OK);
 	}
